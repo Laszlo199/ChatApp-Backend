556f5076f41d02d10caee0904fb09a54d3a66894
kubaczechowski <67064580+kubaczechowski@users.noreply.github.com>
2022-03-13 21:52:30 +0100

    merged two versions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Controller('users')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,11 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { FriendStatus } from './dto/get-users.dto';
 
 @Injectable()
 export class UsersService {
@@ -58,7 +58,9 @@ export class UsersService {
     } else {
       throw new UnauthorizedException('Please check your login credentials');
     }
-  async create(user: CreateUserDto): Promise<User> {
+  }
+
+  async create2(user: CreateUserDto): Promise<User> {
     return await this.userRepository.save(user);
   }
 
