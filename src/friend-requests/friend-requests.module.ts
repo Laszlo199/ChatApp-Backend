5bf41f05a196d1dc9c026240b331edbdca8c2f11
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-03-17 21:17:14 +0100

    wops module

--- src/friend-requests/friend-requests.module.ts
+++ src/friend-requests/friend-requests.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FriendRequest } from './entitites/friend-request.entity';
 import { FriendRequestsService } from './friend-requests.service';
 import { friendRequestGateway } from './friend-request.gateway';
+import { FriendRequestController } from './friend-request.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FriendRequest])],
+  controllers: [FriendRequestController],
   providers: [friendRequestGateway, FriendRequestsService],
 })
 export class FriendRequestsModule {}
