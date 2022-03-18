2b4d343e8984ba80bbe26c4f707c7868066f9e9f
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-03-18 09:08:22 +0100

    Update create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 export class CreateUserDto {
   @ApiProperty()
   username: string;
